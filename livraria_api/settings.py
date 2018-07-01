e6b8b265d076d250cdfcf334c999a983be80f3bd
MushZinho <pabllobeg@hotmail.com>
2018-07-01 12:00:56 -0300

    Fixed Migrations BUG

--- livraria_api/settings.py
+++ livraria_api/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'livraria',
         'USER': 'postgres',
-        'PASSWORD': 'admin',
+        'PASSWORD': 'postgres',
         'HOST': 'localhost',
         'PORT': '5432',
     }
